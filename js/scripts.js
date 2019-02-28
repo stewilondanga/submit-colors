e627ac3c9749cd14ffdfd755ff61af640f0741df
Stewart ilondanga <stilondanga@gmail.com>
2019-02-28 21:03:34 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
 		$('.dd').hide();
 		$('.' + dataName).toggle();
 	});
+})();
+
+(function(){
+  const themeform = document.getElementById("themeform");
+  themeform.onSubmit = (e) => {
+    e.preventDefault();
+    const headerTextColor = document.getElementById('header-text-color').value;
+    const stdTextColor = document.getElementById('standard-text-color').value;
+    const primaryColor = document.getElementById('primary-color').value;
+    const backgroundColor = document.getElementById('background-color').value;
+
+    document.documentElement.style.setProperty('--header-text-color', headerTextColor);
+    document.documentElement.style.setProperty('--standard-text-color', stdTextColor);
+    document.documentElement.style.setProperty('--primary-color', primaryColor);
+    document.documentElement.style.setProperty('--background-color', backgroundColor);
+  };
 })();
