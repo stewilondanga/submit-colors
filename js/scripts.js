ee764ad797351771fe2f11a792602cdcd5773f4b
Stewart ilondanga <stilondanga@gmail.com>
2019-02-28 21:29:14 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,16 @@ var navigate = (function() {
 
 (function() {
     const themeform = document.getElementById("themeform");
-    /*  themeform.onSubmit = (e) => {
-        e.preventDefault();
-        const headerTextColor = document.getElementById('header-text-color').value;
-        const stdTextColor = document.getElementById('standard-text-color').value;
-        const primaryColor = document.getElementById('primary-color').value;
-        const backgroundColor = document.getElementById('background-color').value;
+    themeform.onSubmit = (e) => {
+        /*    e.preventDefault();
+            const headerTextColor = document.getElementById('header-text-color').value;
+            const stdTextColor = document.getElementById('standard-text-color').value;
+            const primaryColor = document.getElementById('primary-color').value;
+            const backgroundColor = document.getElementById('background-color').value;
 
-        document.documentElement.style.setProperty('--header-text-color', headerTextColor);
-        document.documentElement.style.setProperty('--standard-text-color', stdTextColor);
-        document.documentElement.style.setProperty('--primary-color', primaryColor);
-        document.documentElement.style.setProperty('--background-color', backgroundColor);
-      };
-    })();
+            document.documentElement.style.setProperty('--header-text-color', headerTextColor);
+            document.documentElement.style.setProperty('--standard-text-color', stdTextColor);
+            document.documentElement.style.setProperty('--primary-color', primaryColor);
+            document.documentElement.style.setProperty('--background-color', backgroundColor);
+          };
+        })();
