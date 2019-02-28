0ae44c8532cd79338c84c0e18beb8f1a63148097
Stewart ilondanga <stilondanga@gmail.com>
2019-02-28 21:32:04 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,11 @@ var navigate = (function() {
         const headerTextColor = document.getElementById('header-text-color').value;
         const stdTextColor = document.getElementById('standard-text-color').value;
         const primaryColor = document.getElementById('primary-color').value;
-        /*        const backgroundColor = document.getElementById('background-color').value;
-
-                document.documentElement.style.setProperty('--header-text-color', headerTextColor);
-                document.documentElement.style.setProperty('--standard-text-color', stdTextColor);
-                document.documentElement.style.setProperty('--primary-color', primaryColor);
-                document.documentElement.style.setProperty('--background-color', backgroundColor);
-              };
-            })();
+        const backgroundColor = document.getElementById('background-color').value;
+        /*
+                        document.documentElement.style.setProperty('--header-text-color', headerTextColor);
+                        document.documentElement.style.setProperty('--standard-text-color', stdTextColor);
+                        document.documentElement.style.setProperty('--primary-color', primaryColor);
+                        document.documentElement.style.setProperty('--background-color', backgroundColor);
+                      };
+                    })();
